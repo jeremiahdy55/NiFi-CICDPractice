d076588a9deab9960fc63bb5d1248d3a8a780b08
jeremiahdy55 <jeremiahjava55@gmail.com>
2025-08-04 19:44:23 -1000

    try install docker this time on jenkins instance

--- terraform/generate_ansible_files.sh
+++ terraform/generate_ansible_files.sh
@@ -49,10 +49,28 @@ cat <<EOF > "$ANSIBLE_DIR/configure_ec2instances.yml"
           - openjdk-17-jdk
           - unzip
           - curl
-          - docker.io
           - gnupg
           - software-properties-common
           - zip
+        state: present
+
+    - name: Add Docker GPG apt Key
+      apt_key:
+        url: https://download.docker.com/linux/ubuntu/gpg
+        state: present
+
+    - name: Add Docker APT repository
+      apt_repository:
+        repo: deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable
+        state: present
+        update_cache: yes
+
+    - name: Install Docker CE
+      apt:
+        name:
+          - docker-ce
+          - docker-ce-cli
+          - containerd.io
         state: present
 
     - name: Download AWS CLI installer
@@ -116,94 +134,11 @@ cat <<EOF > "$ANSIBLE_DIR/configure_ec2instances.yml"
         name: jenkins
         enabled: yes
         state: started
-
-- name: Configure NiFi
-  hosts: nifi
-  become: true
-  tasks:
-    - name: Install dependencies
-      apt:
-        name: "{{ item }}"
-        state: present
-        update_cache: yes
-      loop:
-        - openjdk-17-jdk
-        - unzip
-        - curl
-EOF
-
-
-# Generate configure_ec2instances.yml
-cat <<EOF > "$ANSIBLE_DIR/configure_ec2instances.yml"
-- name: Configure Jenkins
-  hosts: jenkins
-  become: true
-  tasks:
-    - name: Update and upgrade apt packages
-      apt:
-        update_cache: yes
-        upgrade: dist
-
-    - name: Install required packages
-      apt:
-        name:
-          - openjdk-17-jdk
-          - unzip
-          - curl
-          - gnupg
-          - software-properties-common
-          - zip
-        state: present
-
-    - name: Download AWS CLI installer
-      get_url:
-        url: https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
-        dest: /tmp/awscliv2.zip
-        mode: '0644'
-
-    - name: Unzip AWS CLI installer
-      unarchive:
-        src: /tmp/awscliv2.zip
-        dest: /tmp/
-        remote_src: yes
-
-    - name: Install AWS CLI
-      command: sudo /tmp/aws/install
-      args:
-        creates: /usr/local/bin/aws
-
-    - name: Clean up AWS CLI installer files
-      file:
-        path: "{{ item }}"
-        state: absent
-      loop:
-        - /tmp/awscliv2.zip
-        - /tmp/aws
-
-    - name: Download and save Jenkins GPG keyring
-      get_url:
-        url: https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-        dest: /usr/share/keyrings/jenkins-keyring.asc
-        mode: '0644'
-
-    - name: Add Jenkins repository
-      apt_repository:
-        repo: "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/"
-        filename: jenkins
-        state: present
-        update_cache: yes
-
-    - name: Install Jenkins
-      apt:
-        name: jenkins
-        state: present
-        update_cache: yes
-
-    - name: Enable and start Jenkins service
+      
+    - name: Restart Jenkins to apply Docker group permissions
       systemd:
         name: jenkins
-        enabled: yes
-        state: started
+        state: restarted
 
 - name: Configure NiFi
   hosts: nifi
@@ -219,7 +154,6 @@ cat <<EOF > "$ANSIBLE_DIR/configure_ec2instances.yml"
         - unzip
         - curl
 EOF
-
 
 # Generate start_nifi.yml
 cat <<EOF > "$ANSIBLE_DIR/start_nifi.yml"
